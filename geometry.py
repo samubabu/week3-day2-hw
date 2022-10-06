235396cabc1f385512c76a4a6b510e72ae035228
Sam B <samb@Sams-MacBook-Pro.local>
2022-10-05 23:03:02 -0500

    commit

---
+++ geometry.py
@@ -0,0 +1,12 @@
+from math import pi, sqrt
+
+
+def circle_area(radius):
+    return pi * radius**2
+
+
+
+def hypotenuse (sidea,sideb):
+    return sqrt(sidea**2+sideb**2)
+
+print(hypotenuse(3,4))
---
+++ test_module.py
@@ -0,0 +1,6 @@
+print('Hello this is the test module')
+
+def greet(name):
+    print(f"Hello {name.title()}. How are you today? I hope you are doing well!")
+def leave(name):
+    print(f"Goodbye {name.title()}. It was pleasure seeing you!")
